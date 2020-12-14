818683d317acb5d8dc3422d42d2cb84a8eaa1737
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2020-12-14 14:05:41 +0530

    "Java Variabel"

---
+++ js/test.js
@@ -0,0 +1,24 @@
+// document.write("Hello World <br><br>");
+// document.write("<b>Hello from Piyush Chaturvedi<");
+//here is comment
+
+/*here is 
+some 
+multilines
+*/
+// JavaScript Variable
+// document.write("This are text messages.");
+// document.write("This is a text message");
+// document.write("This is a text message");
+
+// A = "This is are text messages<br>"
+// document.write(A);
+// document.write(A);
+// document.write(A);
+// document.write(A);
+// document.write(A);
+
+// Three types of variable:
+// 1. var
+// 2. Let
+// 3. Const
